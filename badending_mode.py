9a7bb40899cd29aa6be32f21ceca948df5164313
aecong <dkdus78655@gmail.com>
2023-11-16 21:11:39 +0900

    배드엔딩 모드
    r 누르면 playmode 부터 다시 시작
    item개수 초기화, hp 초기화

---
+++ badending_mode.py
@@ -0,0 +1,63 @@
+import random
+
+from pico2d import *
+import game_framework
+
+import game_world
+import item
+import play_mode
+import title_mode
+from background import Badbackground
+from cookie import Cookie
+from hp import Hp
+from sound import Backgroundsound
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_r:
+            game_framework.change_mode(play_mode)
+            Hp.hpCnt = 0
+            Cookie.itemCount = 0
+            cookie.time = get_time()
+        else:
+            cookie.handle_event(event)
+
+
+def init():
+    global running
+    global cookie
+    global background
+    global sound
+
+    sound = Backgroundsound()   # sound 바꾸기
+
+    cookie = Cookie()
+    game_world.add_object(cookie, 1)
+
+    background = Badbackground()
+    game_world.add_object(background, 0)
+
+def finish():
+    game_world.clear()
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause():
+
+    pass
+
+def resume():
+
+    pass
