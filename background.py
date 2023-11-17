be6c3e74ce5aaec33b8c73db922e99e4a23f721f
aecong <dkdus78655@gmail.com>
2023-11-17 21:20:40 +0900

    background 글자 출력

--- background.py
+++ background.py
@@ -17,11 +17,20 @@ class Badbackground:
         self.bgm = load_music('resource/bgm_badending.mp3')
         self.bgm.set_volume(32)
         self.bgm.repeat_play()
+        self.font = load_font('resource/CookieRun Regular.TTF', 32)
+        self.x, self.y = 200, 300
+        self.angle = 0
     def draw(self):
         self.image.draw(400, 300, 800, 600)
+        self.font.draw(self.x, self.y, f'저런... 통과하지 못하셨네요...', (15, 14, 24))
+        self.font.draw(self.x, self.y - 50, f'다시 도전하려면 r키를 누르세요!', (15, 14, 24))
 
     def update(self):
-        pass
+        self.angle += 10
+        if self.angle >= 360:
+            self.angle = 0
+        self.x = 200 + 5 * math.cos(self.angle)
+        self.y = 300 + 5 * math.sin(self.angle)
 
 class Happybackground:
     image = None
