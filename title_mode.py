e8fdac6ae57efb89157845875125e5b0d07a103d
aecong <dkdus78655@gmail.com>
2023-11-17 21:22:05 +0900

    글자 출력 및 글자 사이즈 조절

--- title_mode.py
+++ title_mode.py
@@ -8,8 +8,10 @@ from sound import Titlesound
 
 
 def init():
+    Cookie.start = False
     global titleimage, titlelaser, titlecookie, titlename
-    global sound
+    global sound, size
+    size = 32
     titleimage = load_image('resource/title.png')
     titlelaser = load_image('resource/title_laser.png')
     titlecookie = load_image('resource/title_cookie.png')
@@ -28,6 +30,8 @@ def handle_events():
             game_framework.quit()
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
             Cookie.start = True
+            Cookie.time = 0.0
+            Cookie.jump_sound.play()
             game_framework.change_mode(play_mode)
 
 def draw():
@@ -36,9 +40,17 @@ def draw():
     titlelaser.clip_draw(titlelaser.frame * 851, 0, 851, 101, 540, 290, 851 * 0.75, 101)
     titlecookie.clip_draw(titlecookie.frame * 148, 0, 148, 164, 170, 270, 148 * 2.2, 164 * 2.2)
     titlename.draw(500, 150, 506, 111)
+    global size
+    font = load_font('resource/CookieRun Regular.TTF', size)
+    font.draw(300 - size, 50, f'스페이스바를 누르면 시작!!', (255, 255, 255))
+
     update_canvas()
     delay(0.05)
 
 def update():
     titlelaser.frame = (titlelaser.frame + 1) % 3
     titlecookie.frame = (titlecookie.frame + 1) % 4
+    global size
+    size += 1
+    if size >= 48:
+        size = 32
