2d0788b28135b851dcf6f8c9930e689ab0dc6ddf
aecong <dkdus78655@gmail.com>
2023-11-21 19:11:19 +0900

    game_world 수정

--- game_world.py
+++ game_world.py
@@ -1,11 +1,24 @@
-objects = [[] for _ in range(4)]
+objects = [[] for _ in range(4)]  # 보이는 세계
+
+# 충돌의 세계
+collision_pairs = {}  # {boy:ball}: [ [boy], [ball1, ball2, ball3, ...] ]
 
 
-def add_object(o, depth=0):   # 게임 월드에 객체 추가
+def handle_collisions():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
+
+
+def add_object(o, depth=0):
     objects[depth].append(o)
 
-# def add_objects(ol, depth = 0): # 게임 월드에 객체'들'을 추가
-#     objects[depth] += ol
+
+def add_objects(ol, depth=0):
+    objects[depth] += ol
 
 
 def update():
@@ -20,14 +33,46 @@ def render():
             o.draw()
 
 
+def add_collision_pair(group, a=None, b=None):  # a 와 b 사이의 충돌 검사가 필요하다는 점을 등록
+    if group not in collision_pairs:
+        print(f'New group {group} added...')
+        collision_pairs[group] = [[], []]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+
+
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+
+
 def remove_object(o):
     for layer in objects:
-        if o in layer:  # 객체가 layer 에 들어 있으면
+        if o in layer:
             layer.remove(o)
+            remove_collision_object(o)
+            del o   # 객체 자체를 날리기
             return
-    raise ValueError('존재하지 않는 객체는 지울 수 없음') # 코드 강제 발생
+    raise ValueError('Cannot delete non existing object')
 
 
 def clear():
     for layer in objects:
         layer.clear()
+
+
+def collide(a, b):
+    la, ba, ra, ta = a.get_bb()
+    lb, bb, rb, tb = b.get_bb()
+
+    if la > rb: return False
+    if ra < lb: return False
+    if ta < bb: return False
+    if ba > tb: return False
+
+    return True
