6bd6591d3b8e2334b7b4d02f28421d87f123645f
aecong <dkdus78655@gmail.com>
2023-11-21 18:46:51 +0900

    배경 프레임 시간

--- background.py
+++ background.py
@@ -1,6 +1,19 @@
 from pico2d import load_image
 
+import game_framework
 
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 30.0
+RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
+RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
+RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
+
+# Boy Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 1
+
+FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ACTION
 class Background:
     def __init__(self):
         self.x = 0
@@ -11,6 +24,6 @@ class Background:
         self.image.draw(self.x+2834, 300, 2834, 638)
 
     def update(self):
-        self.x -= 1
+        self.x -= RUN_SPEED_PPS * game_framework.frame_time
         if self.x <= -2834:
             self.x = 0
