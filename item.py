88567883e4c555eb9953796b504cf9da9b7c08ea
aecong <dkdus78655@gmail.com>
2023-11-22 23:30:17 +0900

    속도 조절

--- item.py
+++ item.py
@@ -5,6 +5,7 @@ from pico2d import *
 import game_world
 import game_framework
 from popcorn import Popcorn
+from rod import Rod
 
 PIXEL_PER_METER = (10.0 / 0.3)
 RUN_SPEED_KMPH = 30.0
@@ -12,6 +13,10 @@ RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
 RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
 RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
 
+ROD_SPEED_KMPH = 80.0
+ROD_SPEED_MPM = ROD_SPEED_KMPH * 1000.0 / 60.0
+ROD_SPEED_MPS = ROD_SPEED_MPM / 60.0
+ROD_SPEED_PPS = ROD_SPEED_MPS * PIXEL_PER_METER
 class Item:
     image = None
     item_eat_sound = None
@@ -27,26 +32,29 @@ class Item:
         self.image.draw(self.x, self.y)
 
     def update(self):
-        if Popcorn.eat == 0:
-            self.x -= RUN_SPEED_PPS * game_framework.frame_time
+        if Popcorn.eat == 0 or Rod.Mode == 0 or Rod.Mode == 2 or Rod.Mode == 3:
+            if Rod.Mode == 2 or Rod.Mode == 3:
+                self.x -= ROD_SPEED_PPS * game_framework.frame_time
+            else:
+                self.x -= RUN_SPEED_PPS * game_framework.frame_time
 
         if self.x < 25:
             game_world.remove_object(self)
-
 
     def get_bb(self):
         return self.x - 15, self.y - 15, self.x + 15, self.y + 15
 
     def handle_collision(self, group, other):
-        global count, items
+        global items
         if group == 'cookie:item':
             game_world.remove_object(self)
             Item.item_eat_sound.play()
-            items = [Item(random.randint(800, 1600 - 100), 200, 0) for _ in range(1)]
-            game_world.add_objects(items, 1)
-            for item in items:
-                game_world.add_collision_pair('cookie:item', None, item)
-            return
+            if Rod.Mode == 4:
+                items = [Item(random.randint(800, 1600 - 100), 200, 0) for _ in range(1)]
+                game_world.add_objects(items, 1)
+                for item in items:
+                    game_world.add_collision_pair('cookie:item', None, item)
+                return
         elif group == 'popcorn:item':
             game_world.remove_object(self)
             Item.item_eat_sound.play()
