f1c272280c925fd9fe902743e46bfe7682f32a16
aecong <dkdus78655@gmail.com>
2023-11-23 23:04:28 +0900

    popcorn 클래스에 stateMachine 추가

--- popcorn.py
+++ popcorn.py
@@ -26,15 +26,83 @@ FRAMES_PER_ACTION = 1
 
 FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ACTION
 
+class PowerCount:
+    @staticmethod
+    def enter(popcorn, e):
+        pass
+
+    @staticmethod
+    def exit(popcorn, e):
+        pass
+
+    @staticmethod
+    def do(popcorn):
+
+        pass
+
+    @staticmethod
+    def draw(cookie):
+        popcorn.image.draw(popcorn.x, 200, 100, 100)
+
+
+class Shoot:
+    @staticmethod
+    def enter(popcorn, e):
+        pass
+    @staticmethod
+    def exit(popcorn, e):
+        pass
+    @staticmethod
+    def do(popcorn):
+        pass
+
+    @staticmethod
+    def draw(popcorn):
+        popcorn.image.draw(popcorn.x, 200, 100, 100)
+
+
+class StateMachine:
+    def __init__(self, popcorn):
+        self.popcorn = popcorn
+        self.cur_state = PowerCount
+        self.transitions = {
+            PowerCount: {f_down: Shoot}
+            # Shoot: {time_out: Run},
+        }
+
+    def start(self):
+        self.cur_state.enter(self.popcorn, ('NONE', 0))
+
+    def update(self):
+        self.cur_state.do(self.popcorn)
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(e):
+                if check_event == f_down:
+                    pass
+
+                self.cur_state.exit(self.popcorn, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.popcorn, e)
+                return True
+
+        return False
+
+    def draw(self):
+        self.cur_state.draw(self.popcorn)
+
 class Popcorn:
     image = None
     popcorn_eat_sound = None
     eat = 0
-    throwPower = None
+    throwPower = 0
     def __init__(self):
         if Popcorn.image == None:
             self.image = load_image('resource/popcorn.png')
         self.x, self.y = random.randint(800, 1600), 200
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
         self.power = 1
         self.font = load_font('resource/CookieRun Regular.TTF', 32)
         Popcorn.eat = 0
@@ -43,25 +111,38 @@ class Popcorn:
             Popcorn.popcorn_eat_sound.set_volume(32)
 
     def draw(self):
-        self.image.draw(self.x, 200, 100, 100)
+        self.state_machine.draw()
         # draw_rectangle(*self.get_bb())
         if Popcorn.eat == 1:
             self.font.draw(self.x, self.y + 60, f'{int(self.power):2d}', (255, 0, 255))
 
     def update(self):
-        if Popcorn.eat == 1:
-            self.power += POPCORN_SPEED_PPS * game_framework.frame_time
-            if self.power > 10:
-                self.power = 1
-
-        elif Popcorn.eat == 0:
-            self.x -= RUN_SPEED_PPS * game_framework.frame_time
-
-        if self.x < -100:
-            game_world.remove_object(self)
+        self.state_machine.update()
+        # if Popcorn.eat == 1:
+        #     self.power += POPCORN_SPEED_PPS * game_framework.frame_time
+        #     if self.power > 10:
+        #         self.power = 1
+        # elif Popcorn.eat == 2:
+        #     PIXEL_PER_METER = (10.0 / 0.3)
+        #     POWER_SPEED_KMPH = Popcorn.throwPower * 5
+        #     POWER_SPEED_MPM = POWER_SPEED_KMPH * 1000.0 / 60.0
+        #     POWER_SPEED_MPS = POWER_SPEED_MPM / 60.0
+        #     POWER_SPEED_PPS = POWER_SPEED_MPS * PIXEL_PER_METER
+        #     self.x += POWER_SPEED_PPS * game_framework.frame_time
+        #
+        # elif Popcorn.eat == 0:
+        #     self.x -= RUN_SPEED_PPS * game_framework.frame_time
+        #
+        # if self.x < -100:
+        #     game_world.remove_object(self)
     def get_bb(self):
         return self.x - 25, self.y - 25, self.x + 25, self.y + 25
 
+    def handle_events(self, events):
+        for e in events:
+            if f_down(e):
+                Popcorn.throwPower = self.power
+                Popcorn.eat = 2
     def handle_collision(self, group, other):
         if group == 'cookie:popcorn':
             if Popcorn.eat == 0:
