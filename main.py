1fd7cd86ec96d618e897af982e79642e06bbf7f1
aecong <dkdus78655@gmail.com>
2023-11-25 21:11:53 +0900

    잠깐 실행 시작화면 바꾸기

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pico2d import open_canvas, close_canvas
-# import happyending_mode as start_mode
-import title_mode as start_mode
+import rod_mode as start_mode
+# import title_mode as start_mode
 import game_framework
 
 open_canvas()
