394829b82de6e389a75679982b77badff0ffdbaf
aecong <dkdus78655@gmail.com>
2023-11-26 19:24:22 +0900

    background.py 수정

--- background.py
+++ background.py
@@ -2,12 +2,18 @@ from pico2d import *
 
 import game_framework
 from popcorn import Popcorn
+from rod import Rod
 
 PIXEL_PER_METER = (10.0 / 0.3)
 RUN_SPEED_KMPH = 40.0
 RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
 RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
 RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
+
+ROD_SPEED_KMPH = 80.0
+ROD_SPEED_MPM = ROD_SPEED_KMPH * 1000.0 / 60.0
+ROD_SPEED_MPS = ROD_SPEED_MPM / 60.0
+ROD_SPEED_PPS = ROD_SPEED_MPS * PIXEL_PER_METER
 
 class Badbackground:
     image = None
@@ -78,8 +84,11 @@ class Polejumpbackground:
         self.image.draw(self.x+2240, 300, 1120, 600)
 
     def update(self):
-        if Popcorn.eat == 0:
-            self.x -= RUN_SPEED_PPS * game_framework.frame_time
+        if Popcorn.eat == 0 or Rod.Mode == 0 or Rod.Mode == 2 or Rod.Mode == 3:
+            if Rod.Mode == 2 or Rod.Mode == 3:
+                self.x -= ROD_SPEED_PPS * game_framework.frame_time
+            else:
+                self.x -= RUN_SPEED_PPS * game_framework.frame_time
 
         if self.x <= -1120:
             self.x = 0
