373369510ef7f957cce926d1890297f3cbc6695d
aecong <dkdus78655@gmail.com>
2023-11-28 01:09:57 +0900

    쿠키랑 아이템 충돌
    itemCount 증가

--- cookie.py
+++ cookie.py
@@ -141,6 +141,7 @@ class Cookie:
         self.x, self.y = 100, 200
         self.frame = 0
         self.hp = 100
+        self.itemCount = 0
         self.action = 3  # 0 : 눈빛 점프. 1 : 눈빛 달리기, 2 : 그냥 점프, 3 : 그냥 달리기
         self.state_machine = StateMachine(self)
         self.state_machine.start()
@@ -163,3 +164,7 @@ class Cookie:
     def handle_collision(self, group, other):
         if group == 'cookie:obstacle':
             self.hp -= 1
+        elif group == 'cookie:item':
+            self.itemCount += 1
+            print(self.itemCount)
+            return
