d5ef082f0f6c35e97ffe0fe33a9eb63f44ff123a
aecong <dkdus78655@gmail.com>
2023-11-29 23:25:46 +0900

    프레임마다 속도 다르게 수정

--- rod.py
+++ rod.py
@@ -10,13 +10,22 @@ RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
 RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
 
 MOVE_X_SPEED_KMPH = 40.0
-MOVE_Y_SPEED_KMPH = 80.0
+MOVE_Y_SPEED_KMPH = 90.0
 MOVE_X_SPEED_MPM = MOVE_X_SPEED_KMPH * 1000.0 / 60.0
 MOVE_Y_SPEED_MPM = MOVE_Y_SPEED_KMPH * 1000.0 / 60.0
 MOVE_X_SPEED_MPS = MOVE_X_SPEED_MPM / 60.0
 MOVE_Y_SPEED_MPS = MOVE_Y_SPEED_MPM / 60.0
 MOVE_X_SPEED_PPS = MOVE_X_SPEED_MPS * PIXEL_PER_METER
 MOVE_Y_SPEED_PPS = MOVE_Y_SPEED_MPS * PIXEL_PER_METER
+
+MOVE_X1_SPEED_KMPH = 50.0
+MOVE_Y1_SPEED_KMPH = 120.0
+MOVE_X1_SPEED_MPM = MOVE_X1_SPEED_KMPH * 1000.0 / 60.0
+MOVE_Y1_SPEED_MPM = MOVE_Y1_SPEED_KMPH * 1000.0 / 60.0
+MOVE_X1_SPEED_MPS = MOVE_X1_SPEED_MPM / 60.0
+MOVE_Y1_SPEED_MPS = MOVE_Y1_SPEED_MPM / 60.0
+MOVE_X1_SPEED_PPS = MOVE_X1_SPEED_MPS * PIXEL_PER_METER
+MOVE_Y1_SPEED_PPS = MOVE_Y1_SPEED_MPS * PIXEL_PER_METER
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
@@ -40,35 +49,48 @@ class Rod:
 
     def draw(self):
         if Rod.Mode == 1:
-            self.image.clip_draw(int(self.frame) * 851, 0, 851, 101, self.x, self.y, 340, 40)
+            self.image.clip_draw(int(Rod.frame) * 851, 0, 851, 101, self.x, self.y, 340, 40)
         elif Rod.Mode == 2:
-            self.image.clip_composite_draw(self.size * 851, 0, 851, 101, -self.angle / 2, '', self.x, self.y,340, 40)
+            self.image.clip_composite_draw(int(Rod.size) * 851, 0, 851, 101, -self.angle / 2, '', self.x, self.y,340, 40)
         elif Rod.Mode == 3:
-            self.image.clip_composite_draw(self.size * 851, 0, 851, 101, -self.angle / 2, '', self.x, self.y,340, 40)
+            self.image.clip_composite_draw(int(Rod.size) * 851, 0, 851, 101, -self.angle / 2, '', self.x, self.y,340, 40)
 
 
     def update(self):
         if Rod.Mode == 0:
             Rod.X, Rod.Y = 100, 200
         if Rod.Mode == 1:
-            self.frame = (self.frame + FRAMES_PER_TIME * game_framework.frame_time) % 4
+            Rod.frame = (Rod.frame + FRAMES_PER_TIME * game_framework.frame_time) % 4
         elif Rod.Mode == 2:
+            if self.angle == 0:
+                Rod.X = self.x - 170
             self.angle += 0.1
-            if self.angle > 3.0:
+            if int(Rod.size) == 0 or int(Rod.size) == 2:
+                self.x = 200 + Rod.size * math.cos(self.angle)
+                self.y = 300 + Rod.size * math.sin(self.angle)
+                Rod.X += MOVE_X1_SPEED_PPS * game_framework.frame_time
+                Rod.Y += MOVE_Y1_SPEED_PPS * game_framework.frame_time
+            elif int(Rod.size) == 1 or int(Rod.size) == 3:
+                self.x = 200 + Rod.size * math.cos(self.angle)
+                self.y = 200 + Rod.size * math.sin(self.angle)
+                Rod.X += MOVE_X_SPEED_PPS * game_framework.frame_time
+                Rod.Y += MOVE_Y_SPEED_PPS * game_framework.frame_time
+            if self.angle > 3.5:
                 Rod.Mode = 3
-            self.x = 200 + self.size * math.cos(self.angle)
-            self.y = 200 + self.size * math.sin(self.angle)
-            Rod.X += MOVE_X_SPEED_PPS * game_framework.frame_time
-            Rod.Y += MOVE_Y_SPEED_PPS * game_framework.frame_time
+
         elif Rod.Mode == 3:
             self.angle += 0.1
-            self.x = 200 + self.size * math.cos(self.angle)
-            self.y = 200 + self.size * math.sin(self.angle)
-            Rod.X += MOVE_X_SPEED_PPS * game_framework.frame_time
-            Rod.Y -= MOVE_Y_SPEED_PPS * game_framework.frame_time
-            if self.angle > 6.0:
+            if int(Rod.size) == 0 or int(Rod.size) == 2:
+                self.x = 200 + Rod.size * math.cos(self.angle)
+                self.y = 300 + Rod.size * math.sin(self.angle)
+                Rod.X += MOVE_X1_SPEED_PPS * game_framework.frame_time
+                Rod.Y -= MOVE_Y1_SPEED_PPS * game_framework.frame_time
+            elif int(Rod.size) == 1 or int(Rod.size) == 3:
+                self.x = 200 + Rod.size * math.cos(self.angle)
+                self.y = 200 + Rod.size * math.sin(self.angle)
+                Rod.X += MOVE_X_SPEED_PPS * game_framework.frame_time
+                Rod.Y -= MOVE_Y_SPEED_PPS * game_framework.frame_time
+            if self.angle > 7.0:
                 self.x, self.y = 290, 200
                 self.angle = 0
                 Rod.Mode = 0
-        if Rod.X != 100 and Rod.Y != 200:
-            print(f'{Rod.X, Rod.Y}')
