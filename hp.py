fb72df4a88a2874cc54d8ea7ddec97e894e05317
aecong <dkdus78655@gmail.com>
2023-12-02 01:14:32 +0900

    HP 클래스 생성
    쿠키가 장애물에 닿아 HP가 깎이도록

--- hp.py
+++ hp.py
@@ -8,14 +8,22 @@ RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
 RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
 RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
 
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 3
+
+FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ACTION
+
+
 class Hp:
+    hpCnt = 850
+    x = 0
     def __init__(self):
         self.image = load_image('resource/hp_laser.png')
         self.frame = 0
-        self.hpCnt = 850
+
     def draw(self):
-        self.image.clip_draw(self.frame * 850, 0, 850, 101, 400, 500, self.hpCnt * 0.75, 101)
+        self.image.clip_draw(int(self.frame) * 850, 0, 850, 101, 400 + Hp.x, 500, Hp.hpCnt, 101)
 
     def update(self):
-        self.frame = (self.frame + 1) % 3
-
+        self.frame = (self.frame + FRAMES_PER_TIME * game_framework.frame_time) % 3
