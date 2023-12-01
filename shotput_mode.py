ca18c476cae39af95d7c5575871fb2f6b6b6ca81
aecong <dkdus78655@gmail.com>
2023-12-02 03:24:44 +0900

    배경과 아이템 쿠키만 그대로 옮김

--- shotput_mode.py
+++ shotput_mode.py
@@ -4,13 +4,11 @@ from pico2d import *
 import game_framework
 
 import game_world
-import shotput_mode
 import title_mode
 from background import Background
 from cookie import Cookie
+from hp import Hp
 from item import Item
-from obstacle import Obstacle
-
 
 def handle_events():
     events = get_events()
@@ -19,9 +17,6 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_mode(title_mode)
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_i:
-            if cookie.itemCount >= 30:
-                cookie.itemCount -= 30
         else:
             cookie.handle_event(event)
 
@@ -34,10 +29,19 @@ def init():
 
     cookie = Cookie()
     game_world.add_object(cookie, 1)
+    game_world.add_collision_pair('cookie:item', cookie, None)
+
+    hp = Hp()
+    game_world.add_object(hp, 1)
 
     background = Background()
     game_world.add_object(background, 0)
 
+    global items
+    items = [Item(random.randint(100, 1600 - 100), 200, 0) for _ in range(5)]
+    game_world.add_objects(items, 1)
+    for item in items:
+        game_world.add_collision_pair('cookie:item', None, item)  # 아이템을 등록
 def finish():
     game_world.clear()
 
