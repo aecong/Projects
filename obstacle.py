352672abc5e6c61b737ab9b095bf9793669d569d
aecong <dkdus78655@gmail.com>
2023-12-02 01:15:10 +0900

    장애물과 쿠키가 충돌시 HP 깎이도록

--- obstacle.py
+++ obstacle.py
@@ -4,6 +4,7 @@ from pico2d import load_image, draw_rectangle
 
 import game_framework
 import game_world
+from hp import Hp
 
 PIXEL_PER_METER = (10.0 / 0.3)
 RUN_SPEED_KMPH = 30.0
@@ -11,12 +12,17 @@ RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
 RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
 RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
 
-# Boy Action Speed
+HP_SPEED_KMPH = 5.0
+HP_SPEED_MPM = HP_SPEED_KMPH * 1000.0 / 60.0
+HP_SPEED_MPS = HP_SPEED_MPM / 60.0
+HP_SPEED_PPS = HP_SPEED_MPS * PIXEL_PER_METER
+
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 1
 
 FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ACTION
+
 class Obstacle:
     image = None
 
@@ -41,4 +47,6 @@ class Obstacle:
 
     def handle_collision(self, group, other):
         if group == 'cookie:obstacle':
-            pass
+            Hp.hpCnt -= 1
+            Hp.x -= HP_SPEED_PPS * game_framework.frame_time
+            return
