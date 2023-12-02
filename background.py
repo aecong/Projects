ad25e6212db1626152bb5e133eb389bcf676f7e9
aecong <dkdus78655@gmail.com>
2023-12-02 17:50:26 +0900

    속도 조절

--- background.py
+++ background.py
@@ -1,9 +1,10 @@
-from pico2d import load_image
+from pico2d import *
 
 import game_framework
+from cookie import Cookie
 
 PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 30.0
+RUN_SPEED_KMPH = 40.0 + Cookie.speed
 RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
 RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
 RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
