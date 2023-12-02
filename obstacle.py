d97c9b1cdb829d08498690bd249565b608b8d4a6
aecong <dkdus78655@gmail.com>
2023-12-02 17:52:24 +0900

    쿠키와 쟁애물 충돌시 90도 회전

--- obstacle.py
+++ obstacle.py
@@ -1,12 +1,14 @@
+import math
 import random
 
 from pico2d import load_image, draw_rectangle
 
 import game_framework
 import game_world
+from cookie import Cookie
 
 PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 30.0
+RUN_SPEED_KMPH = 30.0 + Cookie.speed
 RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
 RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
 RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
@@ -24,20 +26,25 @@ class Obstacle:
         if Obstacle.image == None:
             self.image = load_image('resource/obstacle.png')
         self.x, self.y = random.randint(400, 800), 200
-
+        self.falldown = 0
 
     def draw(self):
-        self.image.draw(self.x, 200, 150, 200)
+        if self.falldown == 0:
+            self.image.draw(self.x, 200, 150, 200)
+        else:
+            self.image.clip_composite_draw(0, 0, 150, 200, -math.pi / 2, '', self.x, 150, 150, 200)
 
 
     def update(self):
         self.x -= RUN_SPEED_PPS * game_framework.frame_time
         if self.x < -150:
             self.x = random.randint(600, 1000)
+            self.falldown = 0
 
     def get_bb(self):
         return self.x - 40, self.y - 100, self.x + 40, self.y + 10
 
     def handle_collision(self, group, other):
         if group == 'cookie:obstacle':
+            self.falldown = 1
             pass
